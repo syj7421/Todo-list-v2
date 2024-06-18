13441f76c9d262e70e662151deac581377799983
Sooyoung Jung <91307997+syj7421@users.noreply.github.com>
2024-06-19 04:44:39 +1000

    polish login page using tailwind

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -23,21 +23,50 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username: </label>
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
-        </div>
-        <div>
-          <label>Password: </label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+    <div className="flex min-h-screen items-center justify-center px-6 lg:px-8">
+      <div className="w-full max-w-md">
+        <h1 className="text-center text-3xl font-bold tracking-tight text-gray-900">Welcome !</h1>
+        <form onSubmit={handleSubmit} className="mt-8 space-y-6">
+          <div className="space-y-4">
+            <div>
+              <label className="block text-sm font-medium text-gray-900">Username:</label>
+              <input 
+                type="text" 
+                value={username} 
+                onChange={(e) => setUsername(e.target.value)} 
+                required 
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-900">Password:</label>
+              <input 
+                type="password" 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
+                required 
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+              />
+            </div>
+          </div>
+          <button 
+            type="submit" 
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Login
+          </button>
+        </form>
+        <p class="mt-10 text-center text-sm text-gray-500">
+          Not a user yet?
+          <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"> Sign Up</a>
+        </p>
+      </div>
     </div>
   );
+  
+  
+  
+  
 };
 
 export default LoginPage;
